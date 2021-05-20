9ecf4f13e083a4de50d815808fd797139c423144
JKme <xzhezhe@gmail.com>
2021-05-20 23:05:19 +0800

    update

--- cmd/crack.go
+++ cmd/crack.go
@@ -5,16 +5,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
 func init() {
 	rootCmd.AddCommand(versionCmd)
 }
 
+func runCrack(cmd *cobra.Command, args []string) error {
+	fmt.Println("hello Probe")
+	return nil
+}
+
 var versionCmd = &cobra.Command{
-	Use:   "versions",
-	Short: "Print the version number of Hugo",
-	Long:  `All software has versions. This is Hugo's`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
-	},
+	Use:   "crack",
+	Short: "Brute-force Attack",
+	RunE:  runCrack,
 }

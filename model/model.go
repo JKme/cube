ed7cebe23958976915e97a0950c76ac80afa71e5
JKme <xzhezhe@gmail.com>
2021-05-26 11:43:19 +0800

    develop

--- model/model.go
+++ model/model.go
@@ -1,10 +1,22 @@
 package model
 
+type ProbeTask struct {
+	Ip         string
+	Port       int
+	ScanPlugin string
+}
+
+type ProbeTaskResult struct {
+	ProbeTask ProbeTask
+	Result    string
+	Err       error
+}
+
 type Task struct {
 	Ip         string
 	Port       int
 	ScanPlugin string
-	ScanFunc   string
+	ScanMode   string
 }
 
 type TaskResult struct {

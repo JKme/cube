1bc38f6a66d87f3430de3cfb6e6c55b259c78254
JKme <JKme>
2021-05-28 17:44:06 +0800

    develop

--- plugins/probe/oxid_test.go
+++ plugins/probe/oxid_test.go
@@ -8,10 +8,10 @@ import (
 )
 
 func TestName(t *testing.T) {
-	task := model.ProbeTask{Ip: "192.168.2.226 ", Port: 135, ScanPlugin: "OXID"}
+	task := model.ProbeTask{Ip: "172.20.40.124 ", Port: 135, ScanPlugin: "OXID"}
 	r := OxidProbe(task)
 	fmt.Println(reflect.TypeOf(r.Result))
-	fmt.Printf("Get Result: %v\n", r.Result)
+	fmt.Printf("Get Result: %v\n", r)
 
 	//for _, v := range r.Result {
 	//	fmt.Println( v)

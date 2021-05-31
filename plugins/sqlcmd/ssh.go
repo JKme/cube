1403f553ce2616734f28267bad9ffc25d7299858
JKme <JKme>
2021-05-31 23:02:58 +0800

    develop

--- plugins/sqlcmd/ssh.go
+++ plugins/sqlcmd/ssh.go
@@ -13,14 +13,14 @@ func SshCmd(task model.SqlcmdTask) (result model.SqlcmdTaskResult) {
 	result = model.SqlcmdTaskResult{SqlcmdTask: task, Result: "", Err: nil}
 	config := &ssh.ClientConfig{
 		Timeout: time.Second, //ssh 连接time out 时间一秒钟, 如果ssh验证错误 会在一秒内返回
-		User:    "ubuntu",
+		User:    task.User,
 		Auth: []ssh.AuthMethod{
-			ssh.Password(task.CrackTask.Password),
+			ssh.Password(task.Password),
 		},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(), //这个可以， 但是不够安全
 		//HostKeyCallback: hostKeyCallBackFunc(h.Host),
 	}
-	conn, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", task.CrackTask.Ip, task.CrackTask.Port), config)
+	conn, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", task.Ip, task.Port), config)
 
 	if err == nil {
 		defer conn.Close()
@@ -33,5 +33,4 @@ func SshCmd(task model.SqlcmdTask) (result model.SqlcmdTaskResult) {
 		}
 	}
 	return result
-
 }

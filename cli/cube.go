8f0ebd85ab0b6d4c09dd1b7d5c37cf9ca98c719c
JKme <JKme>
2021-06-01 17:17:03 +0800

    develop

--- cli/cube.go
+++ cli/cube.go
@@ -7,8 +7,6 @@ import (
 	Plugins "cube/plugins"
 	"cube/util"
 	"strings"
-	"sync"
-	"time"
 )
 
 func StartProbeTask(opt *model.ProbeOptions, globalopts *model.GlobalOptions) {
@@ -16,7 +14,15 @@ func StartProbeTask(opt *model.ProbeOptions, globalopts *model.GlobalOptions) {
 	if err != nil {
 		log.Error(err)
 	}
-	tasks := cubelib.GenerateTasks(ips, opt.Port, opt.ScanPlugin)
+	pluginList, err := cubelib.ValidPlugin(opt.ScanPlugin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !cubelib.Subset(pluginList, Plugins.ProbeKeys) {
+		log.Fatalf("plugins not found: %s", pluginList)
+	}
+
+	tasks := cubelib.GenerateTasks(ips, opt.Port, pluginList)
 	cubelib.RunTasks(tasks, globalopts.Threads, globalopts.Timeout)
 }
 
@@ -34,18 +40,4 @@ func StartSqlcmdTask(opt *model.SqlcmdOptions, globalopts *model.GlobalOptions) 
 	task := model.SqlcmdTask{Ip: s.Ip, Port: s.Port, User: opt.User, Password: opt.Password, SqlcmdPlugin: s.Schema, Query: opt.Query}
 	fn := Plugins.SqlcmdFuncMap[task.SqlcmdPlugin]
 	cubelib.SaveSqlcmdReport(fn(task))
-}
-
-func waitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
-	c := make(chan struct{})
-	go func() {
-		defer close(c)
-		wg.Wait()
-	}()
-	select {
-	case <-c:
-		return false // completed normally
-	case <-time.After(timeout):
-		return true // timed out
-	}
 }

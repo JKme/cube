39fa2afc6b291ccb9d81adb021823fad3f2ceafc
JKme <JKme>
2021-06-01 18:17:54 +0800

    format oxid output

--- plugins/probe/oxid_test.go
+++ plugins/probe/oxid_test.go
@@ -3,15 +3,14 @@ package probe
 import (
 	"cube/model"
 	"fmt"
-	"reflect"
 	"testing"
 )
 
 func TestName(t *testing.T) {
 	task := model.ProbeTask{Ip: "172.20.40.100", Port: 135, ScanPlugin: "OXID"}
 	r := OxidProbe(task)
-	fmt.Println(reflect.TypeOf(r.Result))
-	fmt.Printf("Get Result: %v\n", r)
+	//fmt.Println(reflect.TypeOf(r.Result))
+	fmt.Println(r.Result)
 
 	//for _, v := range r.Result {
 	//	fmt.Println( v)

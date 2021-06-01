630f4b233ad6aaaa477fbdc7de28a7394e9597e6
JKme <xzhezhe@gmail.com>
2021-06-01 17:17:03 +0800

    develop

--- cubelib/common_test.go
+++ cubelib/common_test.go
@@ -2,6 +2,7 @@ package cubelib
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -13,4 +14,17 @@ func TestParseService(t *testing.T) {
 	a, err := ParseService("ssh://127.0.0.11")
 
 	fmt.Println(a, err)
+}
+
+func TestSliceContain(t *testing.T) {
+	s := "SMB,SSH"
+	s1 := strings.Split(s, ",")
+	fmt.Println(SliceContain("SMB2", s1))
+}
+
+func TestSameStringSlice(t *testing.T) {
+	s := []string{"SMB", "SSH", "OXID"}
+	s1 := []string{"SMB", "ERR"}
+	//fmt.Println(SameStringSlice(s, s1))
+	fmt.Println(Subset(s1, s))
 }

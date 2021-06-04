4c5e288655a82e1ab62303ec5b51795d950bbd60
JKme <xzhezhe@gmail.com>
2021-06-04 11:13:41 +0800

    dev

--- cubelib/crackTask.go
+++ cubelib/crackTask.go
@@ -1,6 +1,11 @@
 package cubelib
 
-import "cube/model"
+import (
+	"cube/model"
+	Plugins "cube/plugins"
+	"sync"
+	"time"
+)
 
 func GenerateCrackTasks(ip []string, port string, auths []model.Auth, plugins []string) (tasks []model.CrackTask) {
 	tasks = make([]model.CrackTask, 0)
@@ -29,6 +34,39 @@ func generateAuth(user []string, password []string) (authList []model.Auth) {
 		}
 	}
 	return authList
+}
+
+func executeCrackTask(taskChan chan model.CrackTask, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for task := range taskChan {
+		//fmt.Println("Hello")
+		fn := Plugins.ProbeFuncMap[task.ScanPlugin]
+		saveReport(fn(task))
+	}
+
+}
+
+func RunCrackTasks(tasks []model.CrackTask, scanNum int, timeout int) {
+	tasksChan := make(chan model.CrackTask, scanNum*2)
+	var wg sync.WaitGroup
+
+	//消费者
+	wg.Add(scanNum)
+	for i := 0; i < scanNum; i++ {
+		go executeCrackTask(tasksChan, &wg)
+	}
+
+	//生产者
+	//go func() {
+	//
+	//}()
+
+	for _, task := range tasks {
+		tasksChan <- task
+	}
+	close(tasksChan)
+
+	waitTimeout(&wg, time.Duration(timeout)*time.Second)
 }
 
 func StartCrackTask(opt *model.CrackOptions, globalopts *model.GlobalOptions) {

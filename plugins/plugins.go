b5e34ab4c449773176b5367829609e508142dbc3
JKme <JKme>
2021-06-06 00:07:55 +0800

    dev

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -2,21 +2,25 @@ package Plugins
 
 import (
 	"cube/model"
+	"cube/plugins/crack"
 	"cube/plugins/probe"
 	"cube/plugins/sqlcmd"
 )
 
 type ProbeFunc func(task model.ProbeTask) (taskResult model.ProbeTaskResult)
 type SqlcmdFunc func(task model.SqlcmdTask) (taskResult model.SqlcmdTaskResult)
+type CrackFunc func(task model.CrackTask) (taskResult model.CrackTaskResult)
 
 //type CrackFunc func(task model.Task) (taskResult model.TaskResult)
 
 var (
 	ProbeFuncMap  map[string]ProbeFunc
 	SqlcmdFuncMap map[string]SqlcmdFunc
+	CrackFuncMap  map[string]CrackFunc
 
 	ProbeKeys  []string
 	SqlcmdKeys []string
+	CrackKeys  []string
 	//CrackFuncMap map[string]CrackFunc
 )
 
@@ -35,10 +39,9 @@ func init() {
 		SqlcmdKeys = append(SqlcmdKeys, k)
 	}
 
-	//CrackFuncMap = make(map[string]CrackFunc)
-	//CrackFuncMap["SSH"] = crack.SshCrack
-	//var keys []string
-	//for k := range ProbeFuncMap {
-	//	keys = append(keys, k)
-	//}
+	CrackFuncMap = make(map[string]CrackFunc)
+	CrackFuncMap["SSH"] = crack.SshCrack
+	for k := range ProbeFuncMap {
+		CrackKeys = append(CrackKeys, k)
+	}
 }

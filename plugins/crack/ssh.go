6f0556b80c5cfe1e98311cf31b5b411297183c48
JKme <xzhezhe@gmail.com>
2021-06-07 18:31:06 +0800

    dev

--- plugins/crack/ssh.go
+++ plugins/crack/ssh.go
@@ -4,7 +4,7 @@ import "cube/model"
 
 func SshCrack(task model.CrackTask) (result model.CrackTaskResult) {
 	result = model.CrackTaskResult{CrackTask: task, Result: "", Err: nil}
-	result.Result = "Test"
+	result.Result = "[*]: SSH Crack Plugin Done"
 	return result
 
 }

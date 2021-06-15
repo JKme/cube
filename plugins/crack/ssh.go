42423a4ea6c64a9853e0844288eb7d686aa85ad3
JKme <xzhezhe@gmail.com>
2021-06-15 15:01:06 +0800

    信号量解决

--- plugins/crack/ssh.go
+++ plugins/crack/ssh.go
@@ -10,7 +10,7 @@ func SshCrack(task model.CrackTask) (result model.CrackTaskResult) {
 	result = model.CrackTaskResult{CrackTask: task, Result: "", Err: nil}
 	fmt.Printf("Receive Task: %s\n", task)
 	time.Sleep(1 * time.Second)
-	if task.Auth.Password == "111" {
+	if task.Auth.Password == "root" {
 		result.Result = "[*]: SSH Crack Plugin Done"
 	}
 	return result

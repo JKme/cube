e4e6a819beae1381d43bcd04dbc2fe50f886e914
JKme <JKme>
2021-06-17 00:10:09 +0800

    dev

--- plugins/crack/ssh.go
+++ plugins/crack/ssh.go
@@ -10,8 +10,8 @@ func SshCrack(task model.CrackTask) (result model.CrackTaskResult) {
 	result = model.CrackTaskResult{CrackTask: task, Result: "", Err: nil}
 	fmt.Printf("Receive Task: %s\n", task)
 	time.Sleep(2 * time.Second)
-	if task.Auth.Password == "root0" {
-		result.Result = "[*]: SSH Crack Plugin Done"
+	if task.Auth.Password == "root" {
+		result.Result = "SSH Crack Plugin Done"
 	}
 	return result
 

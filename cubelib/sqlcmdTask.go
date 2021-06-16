65311c9ad0c0bc7e15d7cc5f79bb1078cbbefc0a
JKme <JKme>
2021-06-16 18:39:05 +0800

    dev

--- cubelib/sqlcmdTask.go
+++ cubelib/sqlcmdTask.go
@@ -10,7 +10,7 @@ import (
 
 func SaveSqlcmdReport(taskResult model.SqlcmdTaskResult) {
 	if len(taskResult.Result) > 0 {
-		s := fmt.Sprintf("[*]: %s\n[*]: %s:%d\n", taskResult.SqlcmdTask.SqlcmdPlugin, taskResult.SqlcmdTask.Ip, taskResult.SqlcmdTask.Port)
+		s := fmt.Sprintf("[->>>>>]: %s\n[->>>>>]: %s:%d\n", taskResult.SqlcmdTask.SqlcmdPlugin, taskResult.SqlcmdTask.Ip, taskResult.SqlcmdTask.Port)
 		s1 := fmt.Sprintf("[output]:\n%s", taskResult.Result)
 		fmt.Println(s + s1)
 	}

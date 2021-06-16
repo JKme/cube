f0530dba4c4425053c3099d1113188d19d90f9a3
JKme <2935865+JKme@users.noreply.github.com>
2021-06-17 00:10:09 +0800

    dev

--- cubelib/crackTask.go
+++ cubelib/crackTask.go
@@ -1,7 +1,7 @@
 package cubelib
 
 import (
-	"context"
+	"cube/log"
 	"cube/model"
 	Plugins "cube/plugins"
 	"fmt"
@@ -9,19 +9,6 @@ import (
 	"sync"
 	"time"
 )
-
-//func GenerateCrackTasks(ip []string, port string, auths []model.Auth, plugins []string) (tasks []model.CrackTask) {
-//	tasks = make([]model.CrackTask, 0)
-//	for _, i := range ip {
-//		for _, auth := range auths {
-//			for _, p := range plugins {
-//				s := model.CrackTask{Ip: i, Port: port, Auth: &auth, CrackPlugin: p}
-//				tasks = append(tasks, s)
-//			}
-//		}
-//	}
-//	return tasks
-//}
 
 func unitTask(ips []string, auths []model.Auth, plugins []string) (tasks []model.CrackTask) {
 	tasks = make([]model.CrackTask, 0)
@@ -85,46 +72,16 @@ func RunCrackTasks(tasks []model.CrackTask, scanNum int, timeout int) {
 	waitTimeout(&wg, time.Duration(timeout)*time.Second)
 }
 
-func runCrackTask(ctx context.Context, taskChan chan model.CrackTask, resultChan chan model.CrackTaskResult, done chan bool) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case task, ok := <-taskChan:
-			if !ok {
-				done <- true
-				return
-			}
-			fn := Plugins.CrackFuncMap[task.CrackPlugin]
-			r := fn(task)
-			if len(r.Result) > 0 {
-				resultChan <- r
-				done <- true
-			}
-		}
-	}
-}
-
-func monitorResult(done chan bool, resultChan chan model.CrackTaskResult) {
-	for r := range resultChan {
-		if r.Result != "" {
-			fmt.Printf(strings.Repeat("+", 20) + "\n")
-			fmt.Printf("Crack Pass Success:%s\n", r)
-			fmt.Printf(strings.Repeat("+", 20) + "\n")
-			done <- true
-		}
-	}
-}
-
 func saveCrackReport(taskResult model.CrackTaskResult) {
 
 	if len(taskResult.Result) > 0 {
 		k := fmt.Sprintf("%v-%v-%v", taskResult.CrackTask.Ip, taskResult.CrackTask.Port, taskResult.CrackTask.CrackPlugin)
 		h := MakeTaskHash(k)
 		SetTaskHash(h)
-		s := fmt.Sprintf("[*]: %s\n[*]: %s:%s\n", taskResult.CrackTask.CrackPlugin, taskResult.CrackTask.Ip, taskResult.CrackTask.Port)
-		s1 := fmt.Sprintf("[*]: %s", taskResult.Result)
-		fmt.Println(s + s1)
+		s := fmt.Sprintf("[->>>>>]: %s\n[->>>>>]: %s:%s", taskResult.CrackTask.CrackPlugin, taskResult.CrackTask.Ip, taskResult.CrackTask.Port)
+		s1 := fmt.Sprintf("[output]: %s", taskResult.Result)
+		fmt.Println(s)
+		fmt.Println(Fata(s1))
 	}
 }
 
@@ -166,7 +123,61 @@ func runCrack(tasks []model.CrackTask) {
 	waitTimeout(&wg, model.TIMEOUT)
 }
 
-func startCrackTask(plugins []string, ips []string, authList []model.Auth) {
+//func startCrackTask(plugins []string, ips []string, authList []model.Auth) {
+//	tasks := unitTask(ips, authList, plugins)
+//	runCrack(tasks)
+//}
+
+func opt2slice(str string, file string) []string {
+	if len(str) > 0 {
+		r := strings.Split(str, ",")
+		return r
+	}
+	r, _ := FileReader(file)
+	return r
+}
+
+func genPlugins(plugin string) []string {
+	pluginList := strings.Split(plugin, ",")
+	if len(pluginList) > 1 && SliceContain("ALL", pluginList) {
+		log.Fatalf("invalid plugin: %s", plugin)
+	}
+
+	if plugin == "ALL" {
+		pluginList = Plugins.CrackKeys[1:]
+	}
+	return pluginList
+}
+
+func parseOpt(opt *model.CrackOptions) (plugins []string, ips []string, authList []model.Auth) {
+	ip := opt.Ip
+	ipFile := opt.IpFile
+
+	ips = opt2slice(ip, ipFile)
+
+	user := opt.User
+	userFile := opt.UserFile
+	pass := opt.Pass
+	passFile := opt.PassFile
+	us := opt2slice(user, userFile)
+	ps := opt2slice(pass, passFile)
+	for _, u := range us {
+		for _, p := range ps {
+			authList = append(authList, model.Auth{
+				User:     u,
+				Password: p,
+			})
+		}
+	}
+
+	plugin := opt.CrackPlugin
+	plugins = genPlugins(plugin)
+
+	return plugins, ips, authList
+}
+
+func startCrackTask(opt *model.CrackOptions, globalopts *model.GlobalOptions) {
+	plugins, ips, authList := parseOpt(opt)
 	tasks := unitTask(ips, authList, plugins)
 	runCrack(tasks)
 }

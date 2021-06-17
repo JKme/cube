02a8f97b693c744b7de5f255a945ea69405156c0
JKme <JKme>
2021-06-17 23:41:41 +0800

    增加delay的功能，待完善

--- plugins/crack/ssh.go
+++ plugins/crack/ssh.go
@@ -3,13 +3,12 @@ package crack
 import (
 	"cube/model"
 	"fmt"
-	"time"
 )
 
 func SshCrack(task model.CrackTask) (result model.CrackTaskResult) {
 	result = model.CrackTaskResult{CrackTask: task, Result: "", Err: nil}
 	fmt.Printf("Receive Task: %s\n", task)
-	time.Sleep(2 * time.Second)
+	//time.Sleep(2 * time.Second)
 	if task.Auth.Password == "root" {
 		result.Result = "SSH Crack Plugin Done"
 	}

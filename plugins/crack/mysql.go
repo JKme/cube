fcfeebd7bf399d9f24754fabaa808ab9a5a24a54
JKme <xzhezhe@gmail.com>
2021-06-22 14:40:59 +0800

    dev

--- plugins/crack/mysql.go
+++ plugins/crack/mysql.go
@@ -11,11 +11,6 @@ import (
 
 func MysqlCrack(task model.CrackTask) (result model.CrackTaskResult) {
 	result = model.CrackTaskResult{CrackTask: task, Result: "", Err: nil}
-	if !util.PortCheck(task.Ip, task.Port) {
-		return
-	} else {
-		result.Result = fmt.Sprintf("Open:%s", task.Port)
-	}
 
 	dataSourceName := fmt.Sprintf("%v:%v@tcp(%v:%v)/mysql?charset=utf8&timeout=%v", task.Auth.User, task.Auth.Password, task.Ip, task.Port, time.Duration(3)*time.Second)
 	db, err := sql.Open("mysql", dataSourceName)

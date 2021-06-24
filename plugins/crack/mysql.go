5a597ac4e57c155d903a5d8fdafd427a9e9838b0
JKme <xzhezhe@gmail.com>
2021-06-25 00:27:20 +0800

    dev

--- plugins/crack/mysql.go
+++ plugins/crack/mysql.go
@@ -25,7 +25,7 @@ func MysqlCrack(task model.CrackTask) (result model.CrackTaskResult) {
 					if err != nil {
 						fmt.Println(err)
 					}
-					s = s + fmt.Sprintf("OS:%s Version:%s Machine:%s File_Priv:%s\t", cols[1][0:5], cols[0], cols[2], cols[3])
+					s = s + fmt.Sprintf("OS=%s Version=%s Machine=%s File_Priv=%s\t", cols[1], cols[0], cols[2], cols[3])
 
 				}
 				result.Result = util.Green(fmt.Sprintf("User: %s \tPassword: %s \t %s", task.Auth.User, task.Auth.Password, s))

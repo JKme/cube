f369574bc2ec07c1a799bf5ebcaf0d9d8c889b8a
JKme <JKme>
2021-06-24 18:34:43 +0800

    dev

--- plugins/crack/mysql.go
+++ plugins/crack/mysql.go
@@ -6,16 +6,14 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"time"
 )
 
 func MysqlCrack(task model.CrackTask) (result model.CrackTaskResult) {
 	result = model.CrackTaskResult{CrackTask: task, Result: "", Err: nil}
 
-	dataSourceName := fmt.Sprintf("%v:%v@tcp(%v:%v)/mysql?charset=utf8&timeout=%v", task.Auth.User, task.Auth.Password, task.Ip, task.Port, time.Duration(3)*time.Second)
+	dataSourceName := fmt.Sprintf("%v:%v@tcp(%v:%v)/mysql?charset=utf8&timeout=%v", task.Auth.User, task.Auth.Password, task.Ip, task.Port, model.ConnectTimeout)
 	db, err := sql.Open("mysql", dataSourceName)
 	if err == nil {
-		defer db.Close()
 		err = db.Ping()
 		if err == nil {
 			rows, err := db.Query("select @@version, @@version_compile_os, @@version_compile_machine, @@secure_file_priv;")
@@ -33,7 +31,8 @@ func MysqlCrack(task model.CrackTask) (result model.CrackTaskResult) {
 				result.Result = util.Green(fmt.Sprintf("User: %s \tPassword: %s \t %s", task.Auth.User, task.Auth.Password, s))
 			}
 		}
-
+		db.Close()
 	}
+
 	return result
 }

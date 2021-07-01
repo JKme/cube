28f5ebf3dbee1e556613eebc251d4fa29278e22e
JKme <2935865+JKme@users.noreply.github.com>
2021-07-01 13:49:28 +0800

    dev

--- model/vars.go
+++ model/vars.go
@@ -17,13 +17,14 @@ var (
 )
 
 var UserDict = map[string][]string{
-	"ftp":        {"anonymous", "ftp", "admin", "www", "web", "root", "db", "wwwroot", "data"},
-	"mysql":      {"root", "mysql"},
-	"mssql":      {"sa", "sql"},
-	"smb":        {"administrator", "admin", "guest"},
-	"postgresql": {"postgres", "admin"},
-	"ssh":        {"root", "admin"},
-	"mongodb":    {"root", "admin"},
+	"ftp":      {"anonymous", "ftp", "admin", "www", "web", "root", "db", "wwwroot", "data"},
+	"mysql":    {"root", "mysql"},
+	"mssql":    {"sa", "sql"},
+	"smb":      {"administrator", "admin", "guest"},
+	"postgres": {"postgres", "admin"},
+	"ssh":      {"root", "admin"},
+	"mongodb":  {"root", "admin"},
+	"elastic":  {""},
 }
 
 var PassDict = []string{"", "123456", "admin", "admin123", "root", "pass123", "pass@123", "password", "123123", "654321", "111111", "123", "1", "admin@123", "Admin@123", "admin123!@#", "{user}", "{user}1", "{user}111", "{user}123", "{user}@123", "{user}_123", "{user}#123", "{user}@111", "{user}@2019", "P@ssw0rd!", "P@ssw0rd", "Passw0rd", "qwe123", "12345678", "test", "test123", "123qwe!@#", "123456789", "123321", "666666", "a123456.", "123456~a", "000000", "1234567890", "8888888", "!QAZ2wsx", "1qaz2wsx", "abc123", "abc123456", "1qaz@WSX", "a11111", "a12345", "Aa1234", "Aa1234.", "Aa12345", "a123456", "a123123", "Aa123123", "Aa123456", "Aa12345.", "sysadmin", "system", "huawei"}
@@ -38,8 +39,11 @@ func init() {
 	CommonPortMap["smb"] = 445
 	CommonPortMap["ms17010"] = 445
 	CommonPortMap["mssql"] = 1433
+	CommonPortMap["zookeeper"] = 2181
 	CommonPortMap["mysql"] = 3306
+	CommonPortMap["postgres"] = 5432
 	CommonPortMap["redis"] = 6379
+	CommonPortMap["elastic"] = 9200
 	CommonPortMap["mongo"] = 27017
 
 }

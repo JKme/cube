45d79d771421e27527fe03089ee316f306639d73
JKme <JKme>
2021-07-13 17:40:59 +0800

    dev

--- plugins/crack/mssql.go
+++ plugins/crack/mssql.go
@@ -11,7 +11,7 @@ func MssqlCrack(task model.CrackTask) (result model.CrackTaskResult) {
 	result = model.CrackTaskResult{CrackTask: task, Result: "", Err: nil}
 
 	dataSourceName := fmt.Sprintf("server=%v;port=%v;user id=%v;password=%v;database=%v", task.Ip,
-		task.Port, task.Auth.User, task.Auth.Password, "temp")
+		task.Port, task.Auth.User, task.Auth.Password, "tempdb")
 	db, err := sql.Open("mssql", dataSourceName)
 	if err == nil {
 		defer db.Close()

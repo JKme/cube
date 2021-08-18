e563f328baa7d91dd326ca6c834c66742497a031
JKme <2935865+JKme@users.noreply.github.com>
2021-08-18 18:12:35 +0800

    update probe smb plugin

--- plugins/probe/smb_test.go
+++ plugins/probe/smb_test.go
@@ -7,13 +7,14 @@ import (
 )
 
 func TestSMB(t *testing.T) {
-	task := model.ProbeTask{Ip: "172.16.47.3", Port: "445", ScanPlugin: "SMB"}
-	r := smbScan2(task)
-	fmt.Printf("Get Result: %v\n", r.Result)
+	task := model.ProbeTask{Ip: "172.20.40.130", Port: "445", ScanPlugin: "SMB"}
+	r := SmbProbe(task)
+	//fmt.Printf("%v\n", r.Result)
+	fmt.Println(r.Result)
 }
 
-func TestWMI(t *testing.T) {
-	task := model.ProbeTask{Ip: "172.20.40.124", Port: "135", ScanPlugin: "WMI"}
-	wmi(task)
-	//fmt.Printf("Get Result: %v\n", r.Result)
-}
+//func TestWMI(t *testing.T) {
+//	task := model.ProbeTask{Ip: "172.20.40.124", Port: "135", ScanPlugin: "WMI"}
+//	wmi(task)
+//	//fmt.Printf("Get Result: %v\n", r.Result)
+//}

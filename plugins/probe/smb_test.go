45937bcd3ed374fc572a05326105c23a632947b0
JKme <2935865+JKme@users.noreply.github.com>
2021-08-21 00:18:01 +0800

    update

--- plugins/probe/smb_test.go
+++ plugins/probe/smb_test.go
@@ -11,20 +11,20 @@ import (
 
 func TestSMB(t *testing.T) {
 
-	task := model.ProbeTask{Ip: "172.16.157.190", Port: "445", ScanPlugin: "smb"}
+	task := model.ProbeTask{Ip: "192.168.2.226", Port: "445", ScanPlugin: "smb"}
 	r := SmbProbe(task)
 	//fmt.Printf("%v\n", r.Result)
 	fmt.Println(r.Result)
 }
 
 func TestWinrmProbe(t *testing.T) {
-	task := model.ProbeTask{Ip: "172.20.10.21", Port: "5985", ScanPlugin: "smb"}
+	task := model.ProbeTask{Ip: "192.168.2.226", Port: "5985", ScanPlugin: "ntlm-winrm"}
 	r := WinrmProbe(task)
 	fmt.Println(r.Result)
 }
 
 func TestWmiProbe(t *testing.T) {
-	task := model.ProbeTask{Ip: "172.20.10.21", Port: "135", ScanPlugin: "smb"}
+	task := model.ProbeTask{Ip: "192.168.2.226", Port: "135", ScanPlugin: "smb"}
 	r := WmiProbe(task)
 	fmt.Println(r.Result)
 }

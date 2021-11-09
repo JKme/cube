63f68682679e9d46853d12f1483bb3c96feba3e3
JKme <2935865+JKme@users.noreply.github.com>
2021-11-09 14:18:51 +0800

    dev

--- plugins/crack/httpBasic.go
+++ plugins/crack/httpBasic.go
@@ -1,14 +1,19 @@
 package crack
 
 import (
+	"cube/log"
 	"cube/model"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func HttpBasicCrack(task model.CrackTask) (result model.CrackTaskResult) {
 	result = model.CrackTaskResult{CrackTask: task, Result: "", Err: nil}
 	clt := http.Client{}
+	if !strings.HasPrefix(task.Ip, "http://") {
+		log.Errorf("Invalid URL, eg: http://%s", task.Ip)
+	}
 	req, _ := http.NewRequest("POST", task.Ip, nil)
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36")
 	req.Header.Add("Connection", "close")

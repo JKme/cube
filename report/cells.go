8a233866bcd1da8b5dd4881e47fffd2df9901376
JKme <2935865+JKme@users.noreply.github.com>
2022-02-25 18:16:15 +0800

    增加输出CSV

---
+++ report/cells.go
@@ -0,0 +1,48 @@
+package report
+
+//http://dnaeon.github.io/concurrent-maps-and-slices-in-go/
+import "sync"
+
+type CsvCell struct {
+	Ip     string
+	Module string
+	Cell   string
+}
+
+type ConcurrentSlice struct {
+	sync.RWMutex
+	items []CsvCell
+}
+
+type ConcurrentSliceItem struct {
+	Index int
+	Value CsvCell
+}
+
+var CsvCells ConcurrentSlice
+
+func (cs *ConcurrentSlice) Append(item CsvCell) {
+	cs.Lock()
+	defer cs.Unlock()
+
+	cs.items = append(cs.items, item)
+}
+
+func (cs *ConcurrentSlice) Iter() <-chan ConcurrentSliceItem {
+	c := make(chan ConcurrentSliceItem)
+
+	f := func() {
+		cs.Lock()
+		defer cs.Unlock()
+		for index, value := range cs.items {
+			c <- ConcurrentSliceItem{index, value}
+		}
+		close(c)
+	}
+	go f()
+
+	return c
+}
+
+//https://juejin.cn/post/6844904134592692231
+//https://stackoverflow.com/questions/36122668/how-to-sort-struct-with-multiple-sort-parameters
--- report/csv.go
+++
@@ -1,9 +0,0 @@
-package report
-
-type CSV struct {
-	Ip     string
-	Module string
-	Cell   string
-}
-
-//https://juejin.cn/post/6844904134592692231
